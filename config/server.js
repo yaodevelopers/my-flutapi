392abf3bf624e957aa8d2de10bf30f1dd6d7709b
papadaanjua <papadaanjua18@gmail.com>
2020-09-02 13:17:00 +0800

    pertama x deploy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00869d0b2e57b03ef39652d685473e55'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '244d8170-5a93-4005-932e-be3b46c17243'
+};
